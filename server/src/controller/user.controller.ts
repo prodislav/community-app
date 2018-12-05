f7ce0f746da6ab4d35ff3a78490201a3be99450e
Uladzislau_Padolski <Uladzislau_Padolski@epam.com>
2018-12-05 15:31:25 +0300

    feat: added controller for get user role

--- server/src/controller/user.controller.ts
+++ server/src/controller/user.controller.ts
@@ -20,12 +20,12 @@ export class UserController {
   constructor(@inject(UserAuthenticationRepository) private userAuthenticationRepository: UserAuthenticationRepository) {
   }
 
-   /**
-    * @param {Request} request - data from request
-    * @param {UserFieldsToRegister} request.body - user data for registration
-    * after successful validation input data,
-    * controller return user data
-    */
+  /**
+   * @param {Request} request - data from request
+   * @param {UserFieldsToRegister} request.body - user data for registration
+   * after successful validation input data,
+   * controller return user data
+   */
   @httpPost('/register')
   public postRegister(request: Request, response: Response): Promise<Response | User> | Response {
     const { errors, isValid } = validateRegisterInput(request.body);
@@ -41,12 +41,12 @@ export class UserController {
       });
   }
 
-   /**
-    * @param {Request} request - data from request
-    * @param {User} request.body - user data for login
-    * after successful validation input data,
-    * controller return Promise<{ success: boolean, token: string }>
-    */
+  /**
+   * @param {Request} request - data from request
+   * @param {User} request.body - user data for login
+   * after successful validation input data,
+   * controller return Promise<{ success: boolean, token: string }>
+   */
 
   @httpPost('/login')
   public postLogin(request: Request, response: Response): Promise<Response | { success: boolean, token: string }> | Response {
@@ -64,14 +64,14 @@ export class UserController {
       });
   }
 
-   /**
-    * @param {Request} request - data from request
-    * @param {object} request.body - data from request
-    * @param {string} request.body.userEmail - user email
-    * @param {string} request.body.userLanguage - language data
-    * after successful validation input data,
-    * controller return Promise<{ success: boolean, token: string }>
-    */
+  /**
+   * @param {Request} request - data from request
+   * @param {object} request.body - data from request
+   * @param {string} request.body.userEmail - user email
+   * @param {string} request.body.userLanguage - language data
+   * after successful validation input data,
+   * controller return Promise<{ success: boolean, token: string }>
+   */
   @httpPost('/user-language')
   public async userLanguage(request: Request, response: Response): Promise<Response> {
     const userEmail: string = request.body.userEmail;
@@ -96,13 +96,13 @@ export class UserController {
 
   }
 
-   /**
-    * @param {Request} request - data from request
-    * @param {object} request.query - data from request
-    * @param {string} request.query.email - user email
-    * after successful validation email,
-    * controller return user language from db
-    */
+  /**
+   * @param {Request} request - data from request
+   * @param {object} request.query - data from request
+   * @param {string} request.query.email - user email
+   * after successful validation email,
+   * controller return user language from db
+   */
   @httpGet('/get-user-language')
   public async getUserLanguage(request: Request, response: Response): Promise<Response> {
     const userEmail: string = request.query.email;
@@ -123,17 +123,17 @@ export class UserController {
     }
   }
 
-   /**
-    * @param {Request} request - data from request
-    * @param {object} request.body - data from request
-    * @param {string} request.body.email - user email
-    * @param {string} request.body.name - user name
-    * @param {string} request.body.language - user language
-    * @param {string} request.body.accessToken - user accessToken
-    * @param {string} request.body.imageUrl - user imageUrl
-    * after successful validation input data,
-    * controller return Promise<{ success: boolean, token: string }>
-    */
+  /**
+   * @param {Request} request - data from request
+   * @param {object} request.body - data from request
+   * @param {string} request.body.email - user email
+   * @param {string} request.body.name - user name
+   * @param {string} request.body.language - user language
+   * @param {string} request.body.accessToken - user accessToken
+   * @param {string} request.body.imageUrl - user imageUrl
+   * after successful validation input data,
+   * controller return Promise<{ success: boolean, token: string }>
+   */
   @httpPost('/google-auth')
   public async googleAuth(request: Request, response: Response): Promise<Response | { success: boolean, token: string }> {
     const { email, name, language, accessToken, imageUrl } = request.body;
@@ -147,22 +147,20 @@ export class UserController {
     }
   }
 
-   /**
-    * @param {Request} request - data from request
-    * @param {object} request.body - data from request
-    * @param {number} request.body.id - user id
-    * after successful access checking,
-    * controller return status = 200
-    */
-  @httpPost('/isAdmin')
-  public async isAdmin(req: Request, res: Response): Promise<void | Response> {
-    const userId: number = req.body.id;
+  /**
+   * @param {Request} request - data from request
+   * @param {object} request.body - data from request
+   * @param {number} request.body.id - user id
+   * if user with this id is exist then return roleId
+   */
+  @httpGet('/get-user-role')
+  public async getEventsForChange(req: Request, res: Response): Promise<void | Response> {
     try {
-      await this.userAuthenticationRepository.isAdmin(userId);
+      const roleId: number = await this.userAuthenticationRepository.getUserRole(req.body.userId);
 
-      return res.sendStatus(200);
+      return res.status(200).json(roleId);
     } catch (err) {
-      return res.status(500).json(err.message);
+      return res.status(500).json(err.msg);
     }
   }
 }
