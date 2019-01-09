7b27ba38423169a8cee20b97ba593ae37f5d116e
Uladzislau_Padolski <Uladzislau_Padolski@epam.com>
2019-01-09 16:12:33 +0300

    fix: added describe for tests

--- admin/src/App.test.tsx
+++ admin/src/App.test.tsx
@@ -3,8 +3,10 @@ import * as ReactDOM from 'react-dom';
 
 import { App } from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('root test', () =>
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+);
