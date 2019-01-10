dc2ba7620ff3212d232ccf969e0871788d8006af
Uladzislau_Padolski <Uladzislau_Padolski@epam.com>
2019-01-10 11:47:37 +0300

    feat: added logic to events page

--- frontend/src/scenes/Events/Events.model.ts
+++ frontend/src/scenes/Events/Events.model.ts
@@ -1,11 +1,17 @@
 import { History } from 'history';
 
 import {
-    AuthStatus
+    AuthStatus,
+    Event,
+    LoadStatus
 } from 'models';
 
 export interface EventsProps {
     children?: JSX.Element;
     history: History;
     authStatus: AuthStatus;
+    events: Event[];
+    status: LoadStatus;
+
+    initEvents: () => void;
 }
--- frontend/src/scenes/Events/Events.tsx
+++ frontend/src/scenes/Events/Events.tsx
@@ -6,13 +6,20 @@ import {
   AppState,
 } from 'store';
 
+import { isEmpty } from 'utils';
+
 import {
-  AuthStatus,
+  AuthStatus, LoadStatus,
 } from 'models';
 
 import { EventsProps } from './Events.model';
 
 import './Events.scss';
+import { LoadEvents } from 'store/events';
+import {
+  CaSpinner,
+  GithubButton,
+} from 'components';
 
 export class CaEventsPageComponent extends React.Component<EventsProps> {
 
@@ -22,22 +29,53 @@ export class CaEventsPageComponent extends React.Component<EventsProps> {
     if (!isAuthenticated) {
       this.props.history.push('/login');
     }
+
+    this.props.initEvents();
   }
 
+  // TODO: add check events for empty.
+  // TODO: add spinner
+  // TODO: add snackbar
   public render(): JSX.Element {
+    const { events, children, status } = this.props;
     return (
       <div className='ca-about'>
-        {this.props.children}
+        {children}
+        <GithubButton url='https://github.com/js-machine/community-app' />
+        {!isEmpty(events) && (
+          <div className='ca-about__container ca-global-fadeIn'>
+            {events.map((event) => (
+              <div className='ca-about__container-for-event' >
+                <div>{event.title}</div>
+                <div>{event.description}</div>
+                <div>{event.city}</div>
+                <div>{event.place}</div>
+                <div>{event.address}</div>
+                <div>{event.begginingInTime}</div>
+                <div>{event.begginingDate}</div>
+              </div>
+            ))}
+          </div>
+        )}
+        {status === LoadStatus.Fetching && (
+          <div className='ca-homepage__spinner-container'>
+            <CaSpinner isActive={status === LoadStatus.Fetching} />
+          </div>
+        )}
       </div>
     );
   }
 }
 
 const mapStateToProps = (state: AppState) => ({
-  authStatus: state.auth.status
+  authStatus: state.auth.status,
+  events: state.events.events,
+  status: state.events.loadEventsStatus,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch) => ({});
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+  initEvents: () => dispatch(new LoadEvents()),
+});
 
 export const CaEvents = connect(
   mapStateToProps,
